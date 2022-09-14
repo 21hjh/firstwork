fc46df59f962bd552433264e3331ef6540bfde6c
jaehwa hong <ghdwoghk04@sch.ac.kr>
2022-09-14 22:32:45 +0900

    3번문제

--- Project4/problem.cpp
+++ Project4/problem.cpp
@@ -1,20 +1,19 @@
-//#include<iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	int money;
-//	int candy_price;
-//
-//	cout << "현재 가지고 있는돈 :";
-//	cin >> money;
-//	cout << "캔디의 가격:";
-//	cin >> candy_price;
-//
-//	int n_candies = money / candy_price;
-//	cout << "최대로 살 수 있는 캔디의 개수 =" << n_candies << endl;
-//
-//	int change = money % candy_price;
-//	cout << "캔디 구입 후 남은돈 =" << change << endl;
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int money;
+	int candy_price;
+	cout << "현재 가지고 있는돈 :";
+	cin >> money;
+	cout << "캔디의 가격:";
+	cin >> candy_price;
+
+	int n_candies = money / candy_price;
+	cout << "최대로 살 수 있는 캔디의 개수 =" << n_candies << endl;
+
+	int change = money % candy_price;
+	cout << "캔디 구입 후 남은돈 =" << change << endl;
+	return 0;
+}
